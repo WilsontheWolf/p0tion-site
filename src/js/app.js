7e384077926b5d741264ed17024854bd0b62f005
Roembol <roemerantoniepeters@gmail.com>
2021-02-19 17:28:02 +0100

    Get started with some javascript scrolling

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,10 @@
 const header = document.querySelector('header');
+// const menuButtons = document.getElementsByClassName('menu-button');
+// const sections = [
+//   document.querySelector('.about'),
+//   document.querySelector('.download'),
+//   document.querySelector('.credits'),
+// ];
 
 const convertRemToPixels = (rem) => {
   return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
@@ -11,3 +17,11 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scrolled');
   }
 });
+
+// menuButton.addEventListener('click', () => {
+//   sections[menuButtons.indexOf(menuButton)].scrollIntoView();
+// });
+
+const scrollToElement = (elementSelector) => {
+  console.log(elementSelector);
+};
